03f9d27b5a9aeaf19e300ae2799592a396b05c14
Te :D <48310761+imsupsup@users.noreply.github.com>
2023-07-28 17:03:23 +0900

    up2

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -10,21 +10,7 @@ const validateText = (text) => {
 };
 
 const messages = [
-  'hi',
-  'hello',
-  'hola',
-  'you-can-email-me-at-literally-anything! Really',
-  'well, not anything. But most things',
-  'like-this',
-  'or-this',
-  'but not this :(  ',
-  'you.can.also.email.me.with.specific.topics.like',
-  'just-saying-hi',
-  'please-work-for-us',
-  'help',
-  'admin',
-  'or-I-really-like-your-website',
-  'thanks',
+  'tspark408',
 ];
 
 const useInterval = (callback, delay) => {
@@ -82,7 +68,7 @@ const EmailLink = ({ loopMessage }) => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
+      <a href={validateText(message) ? `mailto:${message}@naver.com` : ''}>
         <span>{message}</span>
         <span>@mldangelo.com</span>
       </a>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ hi@mldangelo.com"
+    description="Contact TAESEOP PARK via email @ tspark408@naver.com"
   >
     <article className="post" id="contact">
       <header>
